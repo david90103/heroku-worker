c377bcd8ca6e24c57e8b109a379aa5b19570c369
David Tsai <david90103@gmail.com>
2020-06-01 12:49:03 +0800

    fix view directory

--- src/app.js
+++ src/app.js
@@ -28,7 +28,7 @@ app.use(require('body-parser').urlencoded({
   extended: true
 }));
 app.set('view engine', 'ejs');
-// app.set('views', [path.join(__dirname, '/public'), path.join(__dirname, '../album/public')]);
+app.set('views', [path.join(__dirname, '../public')]);
 app.use(express.static(path.join(__dirname, '../public')));
 
 client.on('error', function (err) {
